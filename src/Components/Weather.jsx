e39cba759528a8a43d8a369c90b192005bd5c8ab
MariamAZ89 <azami.maria89@gmail.com>
2025-01-19 22:30:22 +0100

    modifiche sugli api

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,57 +1,69 @@
-import React, { useEffect } from 'react'
+import * as Icon from 'react-bootstrap-icons';
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
-import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
-import humidity_icon from '../assets/humidity.png'
 
 
 const Weather = () => {
-    const search =async(city)=>{
+    const inputRef = useRef()
+    const[weatherData, setWeatherData] = useState(false);
+    const allIcons ={
+        "01d": Icon.SunFill,
+        "02d": Icon.SunsetFill,
+        "03d": Icon.SunFill,
+        "04d": Icon.SunFill,
+        "05d": Icon.SunFill,
+        "06d": Icon.SunFill,
+        "06d": Icon.SunFill,
+    }
+    }
+    const search = async (city) =>{
         try {
-            const url ='https://api.openweathermap.org/data/2.5/weather?q={city}&appid=${import.meta.env.VITE_APP_ID}';
+            const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
             const response = await fetch(url);
-            const data = await Response.json();   
-            console.log(data);     
+            const data = await response.json();   
+            console.log(data);
+            const icon = allIcons[data.weather[0].icon] || Icon.SunFill;
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
+
         } catch (error) {
             
         }
 
     }
     useEffect(() =>{
-
-    })
-
-  return (
+        search("London");
+    },[])
+{ 
+    return (
     <div className='weather'>
         <div className="search-bar">
-            <input type="text" placeholder='Search . . .'/>
-            <img src={ search_icon } alt="" />
+            <input ref={inputRef} type="text" placeholder=' Search . . .'/>
+            <Icon.Search size={30} color='wihte'/>
         </div>
-        <img src={clear_icon} alt="" className='weather-icon'/>
-        <p className='temperature'>16°C</p>
-        <p className='location'>London</p>
+        <Icon.SunFill  size={80} color='yellow' className='weather-icon'/>
+        <p className='temperature'>{{weatherData.temperature}}°C</p>
+        <p className='location'>{weatherData.location}</p>
         <div className='weather-data'>
             <div className='col'>
-                <img src={humidity_icon} alt="" />
+                <Icon.DropletFill size={30} color="gray" className='icons' />
                 <div>
-                    <p>91%</p>
+                    <p>{weatherData.humidity}%</p>
                     <span>Humidity</span>
                 </div>
                 <div className='col'>
-                <img src={wind_icon} alt="" />
-                <div>
-                    <p>3.6 km/h</p>
-                    <span>Wind Speed</span>
+                    <Icon.Wind size={40} color='gray' className='icons'/>
+                    <div>
+                        <p>{weatherData.windSpeed}km/h</p>
+                        <span>Wind Speed</span>
+                    </div>
                 </div>
-
             </div>
-
-            </div>
-
         </div>
     </div>
   )
